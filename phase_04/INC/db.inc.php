03d71886c59f36658440d059f4d379a59472739c
Nicolas Surleraux <nsurleraux@gmail.com>
2018-05-25 17:08:21 +0200

    Meilleure gestion du nombre de paramètre

--- phase_04/INC/db.inc.php
+++ phase_04/INC/db.inc.php
@@ -45,35 +45,42 @@ class Db
 
     public function call($name, $param = []) {
         $p = [];
+        $argCount = 0;
         switch ($name) {
             //ordonné en ordre décroissant obligatoirement
             // 2 params
-            case 'whoIs': $p[] = '?';
+            case 'whoIs':
+                $argCount = 2;
+                break;
             // 1 param
-            case 'userProfil': $p[]='?';
-            case 'mc_group': $p[]='?';
-            case 'mc_coursesGroup': $p[] = '?';
+            case 'userProfil':
+            case 'mc_group':
+            case 'mc_coursesGroup':
+                $argCount = 1;
             // 0 param
             case 'mc_allGroups':
-                //if (!$this->pdoException) {
-                    try {
-                        $appel = 'call ' . $name . '('. implode(',', $p) .')';
-                        echo $appel;
-                        $sth = $this->iPdo->prepare($appel);
-                        $sth->execute($param);
-                        return $sth->fetchAll(PDO::FETCH_ASSOC);
-
-                    } catch (PDOException $e) {
-                        $this->pdoException = $e;
-                        return ['__ERR__' => $this->pdoException];
-                    }
-                //} else {
-                  //  return ['__ERR__' => $this->pdoException];
-                //}
+                $argCount = 0;
                 break;
             default: return ['__ERR__' => 'call impossible à ' . $name];
 
         }
+        
+        // Et puis seulement on fait la requête
+        //if (!$this->pdoException) {
+            try {
+                $appel = 'call ' . $name . '('. implode(',', array_fill(0, $argCount, '?')); .')';
+                echo $appel;
+                $sth = $this->iPdo->prepare($appel);
+                $sth->execute($param);
+                return $sth->fetchAll(PDO::FETCH_ASSOC);
+
+            } catch (PDOException $e) {
+                $this->pdoException = $e;
+                return ['__ERR__' => $this->pdoException];
+            }
+        //} else {
+          //  return ['__ERR__' => $this->pdoException];
+        //}
 
 
     }
